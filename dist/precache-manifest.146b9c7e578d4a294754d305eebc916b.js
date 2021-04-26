93939a189993de0e75cab3223c003c6fb136a052
吴安乐 <wuanle@qimao.com>
2021-04-26 18:04:38 +0800

    dist

---
+++ dist/precache-manifest.146b9c7e578d4a294754d305eebc916b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c81deaef1d623ae28972",
+    "url": "/css/about.9aacbd92.css"
+  },
+  {
+    "revision": "e0e483e0f81ec2e76d38",
+    "url": "/css/app.4926c23c.css"
+  },
+  {
+    "revision": "d7ff82ffbadb525f735a",
+    "url": "/css/chunk-vendors.662c50cd.css"
+  },
+  {
+    "revision": "2fde05797dc281fff83bc5d71ff314e1",
+    "url": "/img/1.2fde0579.jpg"
+  },
+  {
+    "revision": "da372200f849b15e0095a729aadafcf0",
+    "url": "/img/1.da372200.jpg"
+  },
+  {
+    "revision": "efb8a54721e4bf91006c1179b989b2bc",
+    "url": "/img/1.efb8a547.jpeg"
+  },
+  {
+    "revision": "2b6d34812e578c5abc955699422cc0e1",
+    "url": "/img/2.2b6d3481.jpeg"
+  },
+  {
+    "revision": "4ac1f8b03787efce42bf4527f574b80f",
+    "url": "/img/2.4ac1f8b0.jpg"
+  },
+  {
+    "revision": "4f5e84800954b819756c5fe7ba8364ad",
+    "url": "/img/2.4f5e8480.jpeg"
+  },
+  {
+    "revision": "37d51830941006c787508a950d907c55",
+    "url": "/img/3.37d51830.jpg"
+  },
+  {
+    "revision": "8f02e75f14d930659e08de569a98664f",
+    "url": "/img/3.8f02e75f.jpeg"
+  },
+  {
+    "revision": "eaa2339b98a7866b2ee59670f7b4bcd1",
+    "url": "/img/3.eaa2339b.jpeg"
+  },
+  {
+    "revision": "dfe07046b211e1dcfeab7ba8aa8e0de3",
+    "url": "/img/4.dfe07046.jpg"
+  },
+  {
+    "revision": "13a299da6de16c5f91d76febc437e6ed",
+    "url": "/img/5.13a299da.jpeg"
+  },
+  {
+    "revision": "029ed7d297a01f4bc91ff513f49e3ad2",
+    "url": "/img/trash.029ed7d2.jpg"
+  },
+  {
+    "revision": "bb6afbf9786c9d0e1ad449d9ac08acbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c81deaef1d623ae28972",
+    "url": "/js/about.8a1798f5.js"
+  },
+  {
+    "revision": "e0e483e0f81ec2e76d38",
+    "url": "/js/app.90924a18.js"
+  },
+  {
+    "revision": "d7ff82ffbadb525f735a",
+    "url": "/js/chunk-vendors.5dc62047.js"
+  },
+  {
+    "revision": "5a62b7b3e6ea0c923e71cc39523939f8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
